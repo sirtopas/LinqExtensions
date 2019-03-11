84b629b2522011d6f6f292151112c859135d6168
James <sirtopas@gmail.com>
2019-03-11 14:40:05 +0000

    Initial Commit

---
+++ LinqExtensions/Extensions/LinqExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExtensions.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<MyObject> SortBy(this IEnumerable<MyObject> objects, string sortBy)
+        {
+            switch (sortBy)
+            {
+                case "name":
+                    return objects.OrderBy(o => o.ObjectName);
+                case "id":
+                    return objects.OrderBy(o => o.ObjectId);
+                case "start_date":
+                    return objects.OrderBy(o => o.StartDate);
+                case "end_date":
+                    return objects.OrderBy(o => o.EndDate);
+                default:
+                    return objects.OrderBy(o => o.ObjectName);
+            }
+        }
+    }
+}
---
+++ LinqExtensions/Model/MyObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinqExtensions.Extensions
+{
+    public class MyObject
+    {
+        public int ObjectId { get; set; }
+
+        public string ObjectName { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+    }
+}
